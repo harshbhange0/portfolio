06634b2994296fd5391879ae793ee0f484f0f01a
harshbhanger0 <harshbhange123@gmail.com>
2023-08-23 20:34:02 +0530

    navbar bug fix #2

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import webIcon from "../assets/web_icon.jpg";
+
 import { BsMoonFill, BsSunFill, BsJustify } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
 
 function Navbar({ setTheme }) {
   const [thames, setThames] = useState(false);
@@ -17,107 +18,114 @@ function Navbar({ setTheme }) {
     setTheme(newThames);
   };
 
+  let links = [
+    {
+      title: "Home",
+      url: "#intro",
+    },
+    {
+      title: "About",
+      url: "#about",
+    },
+    {
+      title: "Skills",
+      url: "#skills",
+    },
+    {
+      title: "Projects",
+      url: "#projects",
+    },
+    {
+      title: "Contact",
+      url: "#contact",
+    },
+  ];
+
   return (
     <nav
-      className={`sticky top-0 z-10 flex flex-col  justify-center px-4 py-2 transition-all duration-500 ease-in sm:flex-row sm:justify-between ${
-        thames ? "text-b bg-white shadow-sm" : "bg-[#00005C]"
+      className={`sticky top-0 z-10 flex justify-between  sm:flex-row sm:justify-end ${
+        thames ? "bg-white" : "bg-[#00005C]"
       }`}
     >
-      <div>
-        <button
-          className="block sm:hidden"
-          onClick={() => {
-            const toggleCollapse = !collapse;
-            setCollapse(toggleCollapse);
-          }}
+      <div className="relative z-10 w-[100%]">
+        <div
+          className={`sticky top-0 z-10 flex h-[100%] w-[100%] justify-between py-2 text-2xl sm:static sm:h-auto sm:py-0 ${
+            thames ? "bg-white" : "bg-[#00005C]"
+          }`}
         >
-          <BsJustify className="text-2xl" />
-        </button>
-      </div>
-
-      <ul
-        class={` flex  flex-col justify-center gap-6 py-4 sm:flex-row sm:items-center sm:py-0 ${
-          collapse ? "top-0 " : "absolute top-[-400px] sm:relative sm:top-0"
-        }`}
-      >
-        <li class=" me-auto">
-          <a
-            href="#intro"
-            class={th}
-            onClick={() => {
-              const toggleCollapse = !collapse;
-              setCollapse(toggleCollapse);
-            }}
-          >
-            Home
-          </a>
-        </li>
-        <li class=" me-auto">
-          <a
-            href="#about"
-            class={th}
-            onClick={() => {
-              const toggleCollapse = !collapse;
-              setCollapse(toggleCollapse);
-            }}
-          >
-            About
-          </a>
-        </li>
-        <li class=" me-auto">
-          <a
-            href="#skills"
-            class={th}
-            onClick={() => {
-              const toggleCollapse = !collapse;
-              setCollapse(toggleCollapse);
-            }}
-          >
-            Skills
-          </a>
-        </li>
-        <li class=" me-auto">
-          <a
-            href="#projects"
-            class={th}
-            onClick={() => {
-              const toggleCollapse = !collapse;
-              setCollapse(toggleCollapse);
-            }}
-          >
-            Projects
-          </a>
-        </li>
-        <li class=" me-auto">
-          <a
-            href="#contact"
-            class={th}
-            onClick={() => {
-              const toggleCollapse = !collapse;
-              setCollapse(toggleCollapse);
-            }}
-          >
-            Contact
-          </a>
-        </li>
-        <li className=" flex w-[100%] justify-between sm:gap-4">
-          <button
-            onClick={() => {
-              setThames((prevThames) => !prevThames);
-              toggleThemes();
-            }}
-          >
-            {thames ? (
-              <BsSunFill className={`${th} text-xl`} />
-            ) : (
-              <BsMoonFill className={`${th} text-xl`} />
-            )}
-          </button>{" "}
-          <div className=" overflow-hidden rounded-full">
-            <img className="w-[25px]" src={webIcon} alt="" />
+          <div className="flex items-center ps-4 sm:hidden">
+            <button
+              className={`flex items-center ${
+                thames ? "bg-white" : "bg-[#00005C]"
+              }`}
+              onClick={() => {
+                let toggleCollapse = !collapse;
+                setCollapse(toggleCollapse);
+              }}
+            >
+              {collapse ? <AiOutlineClose /> : <BsJustify />}
+            </button>
           </div>
-        </li>
-      </ul>
+          <div className="flex items-center pe-4 sm:hidden">
+            {" "}
+            <button
+              className="flex items-center"
+              onClick={() => {
+                setThames((prevThames) => !prevThames);
+                toggleThemes();
+              }}
+            >
+              {thames ? (
+                <BsSunFill className={`${th} text-xl`} />
+              ) : (
+                <BsMoonFill className={`${th} text-xl`} />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <ul
+          className={` -z-30 flex w-[100%] flex-col justify-end gap-4 px-4 py-2 transition-all duration-500 ease-in sm:flex-row sm:items-center ${
+            collapse
+              ? "absolute top-[100%] sm:static"
+              : "absolute top-[-500px] sm:static"
+          }
+          ${thames ? "bg-white" : "bg-[#00005C]"}`}
+        >
+          {links.map((link, i) => {
+            return (
+              <li key={i}>
+                <a
+                  className={th}
+                  href={link.url}
+                  onClick={() => {
+                    let toggleCollapse = !collapse;
+                    setCollapse(toggleCollapse);
+                  }}
+                >
+                  {link.title}
+                </a>
+              </li>
+            );
+          })}
+          <li className="hidden sm:block">
+            {" "}
+            <button
+              className="flex items-center"
+              onClick={() => {
+                setThames((prevThames) => !prevThames);
+                toggleThemes();
+              }}
+            >
+              {thames ? (
+                <BsSunFill className={`${th} text-xl`} />
+              ) : (
+                <BsMoonFill className={`${th} text-xl`} />
+              )}
+            </button>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
